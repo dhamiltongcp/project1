33819e686e799a3e3e63c15cc45e91a14d20e8db
dhamiltongcp <50433695+dhamiltongcp@users.noreply.github.com>
2019-07-25 13:00:29 -0400

    Add files via upload

---
+++ js/yr_select.js
@@ -0,0 +1,9 @@
+function fun_a() {
+  document.getElementById("2019").className="open";
+  document.getElementById("2018").className="closed";
+}
+
+function fun_b() {
+  document.getElementById("2019").className="closed";
+  document.getElementById("2018").className="open";
+}
